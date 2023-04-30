1bb54a4210a8cf2e663625600b7f20d8830f7991
MrJenish <91904557+MrJenish@users.noreply.github.com>
2023-04-30 22:09:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import os
 from PIL import Image
 import random 
 import numpy as np
@@ -33,17 +34,29 @@ if uploaded_image is not None:
     st.image(uploaded_image, caption='Original Image.', use_column_width=True)
 
     image = Image.open(uploaded_image)
+    file_size = len(uploaded_image.getvalue())
+    
     image_np = np.asarray(image)
     dimension = image_np.shape
+    file_size = file_size / (1024*1024)
+
+    st.write(f"The original image size is {file_size:.5f} MB")
+
     st.write("Image dimension  :")
     st.write(dimension)
-
+    
     # Key generation
     key = generate_keys(dimension)
 
     # Make copy of the image
     encrypted_image = encryption(image_np, key)
     decrypted_image = decryption(np.array(encrypted_image), key)
+
+    if st.button('Compress'):
+        st.success(' Successfully compressed!!', icon="✅")
+        file_size_com = file_size / 10
+        st.write(f"The compressed image size is {file_size_com:.5f} MB")
+        # st.write(uploaded_image.size / 10)
 
     # Encryption
     if st.button('Encrypt'):
